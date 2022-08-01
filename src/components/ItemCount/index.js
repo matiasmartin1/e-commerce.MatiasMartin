f25b37d676848a0e24cad0652f19b4cf33735309
matiasmartin1 <matiasmartinnnn@gmail.com>
2022-08-01 12:47:19 -0300

    componente itemCount

---
+++ src/components/ItemCount/index.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import './itemCount.css'
+
+export default function ItemCount(props) {
+
+    const [contador, setContador] = useState(props.initial);
+
+    function sumarAlContador() {
+        if (contador === props.stock) {
+            console.log('Maxima cantidad del producto');
+        } else {
+            setContador(contador + 1);
+        }
+    }
+
+    function restarAlContador() {
+        if (contador <= props.initial) {
+            console.log('No puede ser menos de 1')
+        } else {
+            setContador(contador - 1);
+        }
+
+    }
+
+    function onAdd() {
+        console.log('Agregado al carrito');
+    }
+
+    return (
+        <div>
+            <p>Nombre del producto</p>
+            <div className="itemCountButtons">
+
+                    <button type="button" class="btn btn-outline-primary" onClick={restarAlContador}>-</button>
+                    <p>{contador}</p>
+                    <button type="button" class="btn btn-outline-primary" onClick={sumarAlContador}>+</button>
+            </div>
+            <button class="btn btn-outline-primary" type='submit' onClick={onAdd}>Agregar al carrito</button>
+        </div>
+    )
+} 
--- src/components/itemListConteiner/index.js
+++ src/components/itemListConteiner/index.js
@@ -1,7 +1,10 @@
+import ItemCount from "../ItemCount";
+
 export default function ItemListConteiner (props) {
     return (
         <div>
            <p>{props.texto}</p>
+           <ItemCount initial={1} stock={5}/>
         </div>
     )
 }
